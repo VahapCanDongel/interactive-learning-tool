b8fa1a538585bca7d1a971a0fb87350d6afbdf53
VahapCanDongel <vahapcandongel@hotmail.com>
2023-02-10 08:31:12 +0000

    functionality for adding courses is implemented however, it is not submitting anything to the database

--- components/AddCourse.js
+++ components/AddCourse.js
@@ -1,9 +1,33 @@
+import { addDoc, collection, deleteDoc, onSnapshot, query } from "@firebase/firestore";
+import { db } from "../utils/firebase"
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+
 export default function AddCourse({ setAddCategoryVisibility }) {
+  const [course, setCourse] = useState({description: ''})
+  const route = useRouter()
+
+
+  const submitCourses = async (e) => {
+    e.preventDefault()
+    const collectionRef = collection(db, 'courses')
+
+    await addDoc(collectionRef, {
+      ...course
+    })
+
+    setCourse({description: ''})
+    return route.push('/')
+  }
+
   return (
     <div className=" fixed inset-0 z-50 flex justify-center items-center backdrop-blur-sm">
       <div className="w-[400px] h-[300px] bg-gray-900 rounded-md flex justify-center items-center flex-col gap-6 shadow-lg">
         <div className="text-xl text-white">Insert Course</div>
+        <form className="flex justify-center items-center flex-col gap-6" onSubmit={submitCourses}>
         <input
+          value={course.description}
+          onChange={(e) => setCourse({...course, description: e.target.value})}
           className="w-[300px]  p-2 rounded-md bg-transparent border-[1px] border-pink-400 text-white"
           placeholder="Course Name"
         />
@@ -13,6 +37,8 @@ export default function AddCourse({ setAddCategoryVisibility }) {
         >
           Add
         </div>
+        </form>
+       
       </div>
     </div>
   );
